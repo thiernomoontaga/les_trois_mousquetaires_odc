4213fb869b11f7d335f3033f62e959aa1aa2ba4d
Bakary <bakarydiassy28@gmail.com>
2025-04-16 14:14:07 +0000

    Ajout de la page ajouter promotion

---
+++ Dashboard/index.php
@@ -0,0 +1,153 @@
+<link rel="stylesheet" href="dashboard.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
+<div class="conteneur">
+    <div class="tete">
+        <div class="item">
+             <i class="fa-solid fa-graduation-cap"></i>
+             <div class="nombre">
+                180
+                <span>Apprents</span>
+             </div>
+        </div>
+
+        <div class="item">
+             <i class="fa-solid fa-folder"></i>
+             <div class="nombre">
+                5
+                <span>Références</span>
+             </div>
+        </div>
+
+        <div class="item">
+             <i class="fa-solid fa-graduation-cap"></i>
+             <div class="nombre">
+                8
+                <span>Stagiares</span>
+             </div>
+        </div>
+
+        <div class="item">
+             <i class="fa-solid fa-graduation-cap"></i>
+             <div class="nombre">
+                18
+                <span>Permanents</span>
+             </div>
+        </div>
+    </div>
+
+    <div class="statistique">
+        <div class="item1">
+            <i class="fa-solid fa-ellipsis"></i>
+            <img src="logoApp.png" alt="">
+            <div>
+                Transformer la vie des peronnes graces à nos solutions technologiques innovantes et accessibles.
+            </div>
+            <div>
+                <div>
+                    <h2>180</h2> 
+                    <p>Apprenants</p>
+                </div>
+
+                <div class = "separateur"></div>
+
+                <div>
+                    <h2>2025</h2> 
+                    <p>Promotion</p>
+                </div>
+
+                <div class = "separateur"></div>
+
+                <div>
+                    <h2>10</h2> 
+                    <p>Mois</p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="item2">
+            <div class = "tete">
+                <h2>Présences statistiques</h2>
+                <div class = "legende">
+                    <div class="vert">
+                        <div></div>
+                        présences
+                    </div>
+                    <div class="vert-claire">
+                        <div></div>
+                        retard
+                    </div>
+                    <div class="orange">
+                        <div></div>
+                        Absences
+                    </div>
+                    <div class="btn">
+                        Absences
+                        <i class="fa-solid fa-angle-down"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="barre">
+                <div class="barre1">
+                    <span>100</span>
+                    <span>80</span>
+                    <span>60</span>
+                    <span>40</span>
+                    <span>20</span>
+                </div>
+                <div class="jan">
+                    <div></div>
+                    <label for="jan">Jan</label>
+                </div>
+                <div class="Feb">
+                    <div></div>
+                    <label for="feb">Feb</label>
+                </div>
+                <div class="Mar">
+                    <div></div>
+                    <label for="Mar">Mar</label>
+                </div>
+                <div class="Avr">
+                    <div></div>
+                    <label for="Avr">Avr</label>
+                </div>
+                <div class="May">
+                    <div></div>
+                    <label for="May">May</label>
+                </div>
+                <div class="Jun">
+                    <div></div>
+                    <label for="Jun">Jun</label>
+                </div>
+                <div class="Jul">
+                    <div></div>
+                    <label for="Jul">Jul</label>
+                </div>
+                <div class="Aug">
+                    <div></div>
+                    <label for="Aug">Aug</label>
+                </div>
+                <div class="Sep">
+                    <div></div>
+                    <label for="Sep">Sep</label>
+                </div>
+                <div class="Oct">
+                    <div></div>
+                    <label for="Oct">Oct</label>
+                </div>
+                <div class="nov">
+                    <div></div>
+                    <label for="nov">Nov</label>
+                </div>
+                <div class="Dec">
+                    <div></div>
+                    <label for="Dec">Dec</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
